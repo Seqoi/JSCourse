13bbf17f6bc26198ad71453a75bd1d723c1c279b
Botan3301 <darikin123497@mail.ru>
2023-08-22 11:05:36 +0300

    JSCourses

---
+++ app.js
@@ -0,0 +1,15 @@
+//Continue from VSCode /serj/sweet-heart
+
+const person = {
+    firstName:'Serj',
+    lastName:'Vow',
+    year:1997,
+    languages:['ru','en','fr'],
+    hasWife: false,
+    greet: function(){
+        console.log('greet')
+    }
+}
+console.log(person.firstName)
+const key = 'languages'
+console.log(person.languages.length)
